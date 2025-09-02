49d9a8d3515e70b47173a037e53c9b0bc63b566b
mayank <mayankkumars584@gmail.com>
2025-09-02 22:43:49 +0530

    wrote db for user

---
+++ dist/db.d.ts
@@ -0,0 +1,29 @@
+import { Schema } from "mongoose";
+export declare const userModel: import("mongoose").Model<{
+    password?: string | null;
+    username?: string | null;
+}, {}, {}, {}, import("mongoose").Document<unknown, {}, {
+    password?: string | null;
+    username?: string | null;
+}, {}, import("mongoose").DefaultSchemaOptions> & {
+    password?: string | null;
+    username?: string | null;
+} & {
+    _id: import("mongoose").Types.ObjectId;
+} & {
+    __v: number;
+}, Schema<any, import("mongoose").Model<any, any, any, any, any, any>, {}, {}, {}, {}, import("mongoose").DefaultSchemaOptions, {
+    password?: string | null;
+    username?: string | null;
+}, import("mongoose").Document<unknown, {}, import("mongoose").FlatRecord<{
+    password?: string | null;
+    username?: string | null;
+}>, {}, import("mongoose").ResolveSchemaOptions<import("mongoose").DefaultSchemaOptions>> & import("mongoose").FlatRecord<{
+    password?: string | null;
+    username?: string | null;
+}> & {
+    _id: import("mongoose").Types.ObjectId;
+} & {
+    __v: number;
+}>>;
+//# sourceMappingURL=db.d.ts.map
