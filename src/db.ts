51d947c2e4041c8f310f50be1c9b20ebacc053fe
mayank <mayankkumars584@gmail.com>
2025-09-08 12:19:18 +0530

    added content schema

--- src/db.ts
+++ src/db.ts
@@ -1,4 +1,4 @@
-import { model,Schema } from "mongoose";
+import mongoose, { model,Schema } from "mongoose";
 
 const userSchema= new Schema({
     username:{type: String ,unique:true},
@@ -6,3 +6,12 @@ const userSchema= new Schema({
 })
 
 export const userModel= model("users",userSchema);
+
+const contentSchema=new Schema({
+    title:String,
+    link:String,
+    tags:[{type:mongoose.Types.ObjectId,ref:'tag'}],
+    userId:[{type:mongoose.Types.ObjectId,ref:'tag',required:'true'}]
+})
+
+export const contentModel= model("contents",contentSchema);
